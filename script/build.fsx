fbdc8f36f7db7f61dbd5b88b8357e928e15b0650
Eric Levine <eric.levine@apx-labs.com>
2016-05-27 11:03:31 -0400

    Also building and publishing the debug build

--- script/build.fsx
+++ script/build.fsx
@@ -34,6 +34,7 @@ let cbForestProject = "src"</>"Couchbase.Lite.Shared"</>"vendor"</>"cbforest"</>
 // nuget related paths
 let nugetPath = Path.GetFullPath(@"./src/.nuget/Nuget.Exe")
 let nuspecPath = Path.GetFullPath(@"./packaging/nuget/couchbase-lite-apx.nuspec")
+let debugNuspecPath = Path.GetFullPath(@"./packaging/nuget/couchbase-lite-apx-debug.nuspec")
 let artifactsNuGetDir = Path.GetFullPath(@"./artifacts/nuget/")
 let artifactsBuildDir = Path.GetFullPath(@"./artifacts/build/")
 
@@ -43,54 +44,70 @@ Target "Bootstrap"(fun _ ->
 )
 
 Target "Build" (fun _ ->
+    let mutable restoreFailureHandled = false;
+
     // iterate through the solutions and restore their packages
     [
         net45Solution
         androidSolution
     ]
-    |> List.iter(fun solution -> 
-        // restore packages for the current solution
-        RestoreMSSolutionPackages(fun p ->
-            {p with
-                OutputPath = "src/packages"
-            })
-            solution
+    |> List.iter(fun solution ->
+        try
+            // restore packages for the current solution
+            RestoreMSSolutionPackages(fun p ->
+                {p with
+                    OutputPath = "src/packages"
+                })
+                solution
+        with
+            | Failure msg ->
+                // there is a failure restoring packages for one of the samples
+                // Handling it here allows us to proceed with the build
+                log msg
+                restoreFailureHandled <- true
         )
-    
-  
+
+
     // build the projects
     [
         cbForestProject
         net45Project
-        androidProject 
-    ] 
-    |> List.iter(fun proj ->  build setParams proj |> DoNothing) 
+        androidProject
+    ]
+    |> List.iter(fun proj ->  build setParams proj |> DoNothing)
 
+    //// Workaround for https://github.com/fsharp/FAKE/issues/1079
+    if restoreFailureHandled then traceEndTask "Build" "Error restoring packages was handled"
 )
 
 Target "Package" (fun _ ->
     ensureDirectory artifactsNuGetDir
     ensureDirectory artifactsBuildDir
-    
+
     let nuspecProps = getNuspecProperties(File.ReadAllText(nuspecPath))
-        
+    let debugNuspecProps = getNuspecProperties(File.ReadAllText(debugNuspecPath))
+
     let tcBuildValue = match TeamCityBuildNumber with
                        | Some(num) -> num
                        | None -> ""
 
     let version = sprintf "%s%s-apx" nuspecProps.Version tcBuildValue
-    
+    let debugVersion = sprintf "%s%s-debug-apx" nuspecProps.Version tcBuildValue
+
     let basePath =  Path.GetFullPath "."
     // create and execute the command ourselves, since the NuGet helper doesn't allow us to set the BasePath command argument which we need
     let commandArgs = sprintf @"pack -Verbosity detailed -BasePath %s -Version %s -OutputDirectory %s %s" basePath version artifactsNuGetDir nuspecPath
     let result = Shell.Exec(nugetPath,  commandArgs.ToString())
 
+    let debugCommandArgs = sprintf @"pack -Verbosity detailed -BasePath %s -Version %s -OutputDirectory %s %s" basePath debugVersion artifactsNuGetDir debugNuspecPath
+    let debugResult = Shell.Exec(nugetPath,  debugCommandArgs.ToString())
+
     if result <> 0 then failwithf "%s exited with error %d" "build.bat" result
 )
 
 Target "TeamCity"(fun _ ->
-    directoryInfo artifactsNuGetDir 
-        |> filesInDir 
+    directoryInfo artifactsNuGetDir
+        |> filesInDir
         |> Array.iter(fun file -> PublishArtifact file.FullName)
 )
 
